31f0da17a103c8bf978b36c5d9823befa14d8dca
daisnurfaizi <ds_nurfaizi@gmail.com>
2020-09-21 22:04:13 +0700

    create migration books

--- simpleLibrary/database/migrations/2020_09_21_145820_create_authors_table.php
+++ simpleLibrary/database/migrations/2020_09_21_145820_create_authors_table.php
@@ -14,8 +14,8 @@ class CreateAuthorsTable extends Migration
     public function up()
     {
         Schema::create('authors', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->string('name');
         });
     }
 
