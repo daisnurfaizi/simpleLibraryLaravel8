3e7e4dc66581952f4a9b16a0cb5755ac3a444923
daisnurfaizi <ds_nurfaizi@gmail.com>
2020-09-21 12:43:00 +0700

    add side-bar admin for template

--- simpleLibrary/resources/views/admin/home.blade.php
+++ simpleLibrary/resources/views/admin/home.blade.php
@@ -21,5 +21,18 @@
         </div>
     </header>
     {{-- end Header --}}
-</body>
+    {{-- sidebar --}}
+    <div class="sidebar">
+        <div class="profil-info">
+            <img src="#" class="profil-image" alt=""/>
+            <h4>Admin</h4>
+        </div>
+        <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
+        <a href=" #"><i class="far fa-user-circle"></i><span>Profile</span></a>
+        <a href="#"><i class="fas fa-bell"></i><span>Notification</span></a>
+        <a href="#"><i class="far fa-envelope"></i><span>Messages</span></a>
+        <a href="#"><i class="fas fa-tasks"></i><span>Settings</span></a>
+    </div>
+    {{-- end sidebar --}}
+    </body>
 </html>
