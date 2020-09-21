608114330bd9a86cdaef26872e1186e86fdaca75
daisnurfaizi <ds_nurfaizi@gmail.com>
2020-09-21 12:37:26 +0700

    add header admin for temlate

--- simpleLibrary/resources/views/admin/home.blade.php
+++ simpleLibrary/resources/views/admin/home.blade.php
@@ -7,6 +7,19 @@
     <title>Home</title>
 </head>
 <body>
-    
+    <input type="checkbox" id="check"/>
+    {{-- header --}}
+    <header>
+        <label for="check">
+            <i class="fas fa-bars" id="sidebar-btn"></i>
+        </label>
+        <div class="left-area">
+            <h3>Library <span>Simple</span></h3>
+        </div>
+        <div class="right-class">
+            <a href="#" class="logout-btn">Logout</a>
+        </div>
+    </header>
+    {{-- end Header --}}
 </body>
 </html>
