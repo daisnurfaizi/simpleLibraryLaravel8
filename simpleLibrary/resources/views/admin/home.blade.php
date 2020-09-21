64cc99c71f44dd4ac50e1cbc0fde041810393341
daisnurfaizi <ds_nurfaizi@gmail.com>
2020-09-21 13:30:15 +0700

    add head templates partials

--- simpleLibrary/resources/views/admin/home.blade.php
+++ simpleLibrary/resources/views/admin/home.blade.php
@@ -1,13 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
-    <link href="{{ asset('assets/admin-style.css') }}" media="all" rel="stylesheet" type="text/css" />
-    <title>Home</title>
-</head>
+    @include('admin.templates.partials.head')
 <body>
     <input type="checkbox" id="check"/>
     {{-- header --}}
