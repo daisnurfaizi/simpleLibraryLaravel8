2794397d55e326820581891876e1ded5758b51ab
daisnurfaizi <ds_nurfaizi@gmail.com>
2020-09-21 15:25:42 +0700

    change sidebar menu

--- simpleLibrary/resources/views/admin/templates/partials/sidebar.blade.php
+++ simpleLibrary/resources/views/admin/templates/partials/sidebar.blade.php
@@ -1,11 +1,11 @@
-<div class="sidebar">
-    <div class="profil-info">
-        <img src="#" class="profil-image" alt=""/>
-        <h4>Admin</h4>
+ <div class="sidebar">
+        <div class="profil-info">
+            <img src="#" class="profil-image" alt=""/>
+            <h4>Admin</h4>
+        </div>
+        <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
+        <a href=" #"><i class="far fa-user-circle"></i><span>Books</span></a>
+        <a href="#"><i class="fas fa-bell"></i><span>Users</span></a>
+        <a href="#"><i class="far fa-envelope"></i><span>Authors</span></a>
+        <a href="#"><i class="fas fa-tasks"></i><span>Settings</span></a>
     </div>
-    <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
-    <a href=" #"><i class="far fa-user-circle"></i><span>Profile</span></a>
-    <a href="#"><i class="fas fa-bell"></i><span>Notification</span></a>
-    <a href="#"><i class="far fa-envelope"></i><span>Messages</span></a>
-    <a href="#"><i class="fas fa-tasks"></i><span>Settings</span></a>
-</div>
