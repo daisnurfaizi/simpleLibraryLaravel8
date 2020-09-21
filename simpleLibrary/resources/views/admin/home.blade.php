4fedd62c2d98505393a452721b36b396ef9799a7
daisnurfaizi <ds_nurfaizi@gmail.com>
2020-09-21 13:33:32 +0700

    add sidebar templates partials

--- simpleLibrary/resources/views/admin/home.blade.php
+++ simpleLibrary/resources/views/admin/home.blade.php
@@ -7,17 +7,7 @@
     @include('admin.templates.partials.header')
     {{-- end Header --}}
     {{-- sidebar --}}
-    <div class="sidebar">
-        <div class="profil-info">
-            <img src="#" class="profil-image" alt=""/>
-            <h4>Admin</h4>
-        </div>
-        <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
-        <a href=" #"><i class="far fa-user-circle"></i><span>Profile</span></a>
-        <a href="#"><i class="fas fa-bell"></i><span>Notification</span></a>
-        <a href="#"><i class="far fa-envelope"></i><span>Messages</span></a>
-        <a href="#"><i class="fas fa-tasks"></i><span>Settings</span></a>
-    </div>
+    @include('admin.templates.partials.sidebar')
     {{-- end sidebar --}}
     {{-- content --}}
     <div class="content">
