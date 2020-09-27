b024045f58d0bb04f9682b589f9994794bb5c2f1
daisnurfaizi <ds_nurfaizi@gmail.com>
2020-09-28 00:20:49 +0700

    change icon sidebar

--- simpleLibrary/resources/views/admin/templates/partials/sidebar.blade.php
+++ simpleLibrary/resources/views/admin/templates/partials/sidebar.blade.php
@@ -4,8 +4,8 @@
             <h4>Admin</h4>
         </div>
         <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
-        <a href=" #"><i class="far fa-user-circle"></i><span>Books</span></a>
-        <a href="#"><i class="fas fa-bell"></i><span>Users</span></a>
-        <a href="#"><i class="far fa-envelope"></i><span>Authors</span></a>
+        <a href=" #"><i class="fa fa-book"></i><span>Books</span></a>
+        <a href="#"><i class="fa fa-users"></i><span>Users</span></a>
+        <a href="#"><i class="fas fa-user-circle""></i><span>Authors</span></a>
         <a href="#"><i class="fas fa-tasks"></i><span>Settings</span></a>
     </div>
