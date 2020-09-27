9547946fe47841128628a28cc091ad12fbfae07d
daisnurfaizi <ds_nurfaizi@gmail.com>
2020-09-28 00:24:45 +0700

    fix authors icon

--- simpleLibrary/resources/views/admin/templates/partials/sidebar.blade.php
+++ simpleLibrary/resources/views/admin/templates/partials/sidebar.blade.php
@@ -6,6 +6,6 @@
         <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
         <a href=" #"><i class="fa fa-book"></i><span>Books</span></a>
         <a href="#"><i class="fa fa-users"></i><span>Users</span></a>
-        <a href="#"><i class="fas fa-user-circle""></i><span>Authors</span></a>
+        <a href="#"><i class="fas fa-user-circle"></i><span>Authors</span></a>
         <a href="#"><i class="fas fa-tasks"></i><span>Settings</span></a>
     </div>
